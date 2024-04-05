56fb1e3fa43d41d644b7be88465caa380a60c99d
Suyog <Suyog@123>
2024-04-05 12:26:02 +0530

    Github Setup is done

---
+++ src/main/java/com/codewithzosh/ecommerce/EcommerceBackendApplication.java
@@ -0,0 +1,14 @@
+package com.codewithzosh.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendApplication.class, args);
+		System.out.println("Hellow World");
+	}
+
+}
